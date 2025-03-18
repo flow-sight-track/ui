4ac62db03a33c6bba65b6a4e68fde905f1f18082
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-18 09:15:21 -0300

    refactor SignInPage to include Card component for improved layout

--- src/app/(root)/(auth)/signin/page.tsx
+++ src/app/(root)/(auth)/signin/page.tsx
@@ -1,17 +1,19 @@
 import NextLink from 'next/link'
 
-import { Button, Link, Stack, TextField } from '@mui/material'
+import { Button, Card, Link, Stack, TextField } from '@mui/material'
 
 const SignInPage = () => {
   return (
-    <Stack spacing={2} className="w-full max-w-sm">
-      <TextField type="email" label="E-mail" variant="outlined" />
-      <TextField type="password" label="Senha" variant="outlined" />
-      <Button variant="contained">Login</Button>
-      <Link href="/signup" component={NextLink} className="self-center">
-        Não tem uma conta? Crie uma
-      </Link>
-    </Stack>
+    <Card className="w-full max-w-sm p-6">
+      <Stack spacing={2} className="w-full max-w-sm">
+        <TextField type="email" label="E-mail" variant="outlined" />
+        <TextField type="password" label="Senha" variant="outlined" />
+        <Button variant="contained">Login</Button>
+        <Link href="/signup" component={NextLink} className="self-center">
+          Não tem uma conta? Crie uma
+        </Link>
+      </Stack>
+    </Card>
   )
 }
 
