7dd9e26560d7973f46dc5b10a406482cbdda58d1
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-18 18:20:27 -0300

    fetch user data in dashboard component

--- src/app/(root)/(routes)/dashboard/page.tsx
+++ src/app/(root)/(routes)/dashboard/page.tsx
@@ -1,4 +1,9 @@
-const Dashboard = () => {
+import getMe from '@/modules/auth/actions/auth/get-user-me'
+
+const Dashboard = async () => {
+  const me = await getMe()
+  console.log(me)
+
   return (
     <div>
       <h1>Dashboard</h1>
