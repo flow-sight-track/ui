fb2a4b0d4fcca54333a455c28927d92e9dac9dd2
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-18 18:20:07 -0300

    refactor sign-in page to use react-hook-form for form handling and validation

--- src/app/(root)/(auth)/signin/page.tsx
+++ src/app/(root)/(auth)/signin/page.tsx
@@ -1,19 +1,67 @@
+'use client'
+
 import NextLink from 'next/link'
+
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { Button, Card, Link, Stack, TextField } from '@mui/material'
 
+import loginUser from '@/modules/auth/actions/auth/login-user'
+import { LoginUser } from '@/modules/auth/types/auth/login-user'
+import { LoginUserSchema } from '@/modules/auth/schemas/auth/login-user-schema'
+
 const SignInPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: zodResolver(LoginUserSchema)
+  })
+
+  const onSubmit = async (data: LoginUser): Promise<void> => {
+    try {
+      const formData = new FormData()
+      formData.append('email', data.email)
+      formData.append('password', data.password)
+      await loginUser(null, formData)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <Card className="w-full max-w-sm p-6">
-      <Stack spacing={2} className="w-full max-w-sm">
-        <TextField type="email" label="E-mail" variant="outlined" />
-        <TextField type="password" label="Senha" variant="outlined" />
-        <Button variant="contained">Login</Button>
-        <Link href="/signup" component={NextLink} className="self-center">
-          Não tem uma conta? Crie uma
-        </Link>
-      </Stack>
-    </Card>
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md">
+      <Card className="p-6">
+        <Stack spacing={2}>
+          <TextField
+            {...register('email')}
+            name="email"
+            type="email"
+            label="E-mail"
+            variant="outlined"
+            helperText={errors.email?.message}
+            error={!!errors.email}
+          />
+          <TextField
+            {...register('password')}
+            name="password"
+            type="password"
+            label="Senha"
+            variant="outlined"
+            helperText={errors.password?.message}
+            error={!!errors.password}
+          />
+          <Button type="submit" variant="contained">
+            Login
+          </Button>
+          <Link href="/signup" component={NextLink} className="self-center">
+            Não tem uma conta? Crie uma
+          </Link>
+        </Stack>
+      </Card>
+    </form>
   )
 }
 
